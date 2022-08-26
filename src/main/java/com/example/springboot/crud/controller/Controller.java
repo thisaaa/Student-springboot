7b670646af84b1a7dd3efafd155bf97a0258f2a7
thisaaa <thisarainduwara99@outlook.com>
2022-08-26 14:09:39 +0530

    change the controller name

--- src/main/java/com/example/springboot/crud/controller/Controller.java
+++ src/main/java/com/example/springboot/crud/controller/Controller.java
@@ -1,7 +1,10 @@
 package com.example.springboot.crud.controller;
 
+import com.example.springboot.crud.entity.Course;
 import com.example.springboot.crud.entity.Student;
-import com.example.springboot.crud.serviceImplementation.StudentServiceImpl;
+//import com.example.springboot.crud.serviceImplementation.CourseServiceImpl;
+//import com.example.springboot.crud.serviceImplementation.CourseServiceImpl;
+import com.example.springboot.crud.serviceImplementation.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,33 +12,41 @@ import java.util.List;
 
 @RestController
 @CrossOrigin("*")
-public class UserController {
+public class Controller {
     @Autowired
-    private StudentServiceImpl studentServiceImpl;
+    private ServiceImpl serviceImpl;
+
+
+    //to save new course
+    @PostMapping(value = "saveCourse")
+    public Course saveCourse(@RequestBody Course course){
+        serviceImpl.saveCourse(course);
+        return course;
+    }
 
     //to save new student
     @PostMapping(value = "saveStudent")
     public Student saveStudent(@RequestBody Student student){
-        studentServiceImpl.saveStudent(student);
+        serviceImpl.saveStudent(student);
         return student;
     }
 
     //to get all student details
     @GetMapping(value = "getAllStudents")
     public List<Student> findAllStudent(){
-        return studentServiceImpl.findAllStudents();
+        return serviceImpl.findAllStudents();
     }
 
     //to update student details
     @PutMapping(value = "updateStudent")
     public Student updateStudent(@RequestBody Student student){
-        return studentServiceImpl.updateStudent(student);
+        return serviceImpl.updateStudent(student);
     }
 
     //to delete student details
     @DeleteMapping(value = "deleteStudent")
     public String deleteStudent(@RequestParam int id){
-        studentServiceImpl.deleteStudent(id);
+        serviceImpl.deleteStudent(id);
         return "Student Deleted !";
     }
 }
